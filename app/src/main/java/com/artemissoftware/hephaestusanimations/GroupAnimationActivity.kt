987f6506c00a5a1461f7ee26afb1b91fe4524c8c
gustavo <gustavo.isaac@gmail.com>
2021-10-30 19:59:27 +0100

    A Simple Fade Animation Using ConstraintLayout

---
+++ app/src/main/java/com/artemissoftware/hephaestusanimations/GroupAnimationActivity.kt
@@ -0,0 +1,36 @@
+package com.artemissoftware.hephaestusanimations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.transition.Fade
+import android.transition.Transition
+import android.transition.TransitionManager
+import android.view.View
+import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.Group
+
+
+class GroupAnimationActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_group_animation)
+
+        val group = findViewById(R.id.group) as Group
+        val start = findViewById(R.id.start) as Button
+        val rootLayout = findViewById(R.id.rootLayout) as ConstraintLayout
+
+        start.setOnClickListener {
+            val fade: Transition = Fade()
+            fade.setDuration(1500)
+            TransitionManager.beginDelayedTransition(rootLayout, fade)
+            start.setVisibility(View.GONE)
+            group.setVisibility(View.VISIBLE)
+
+        }
+
+
+    }
+}
